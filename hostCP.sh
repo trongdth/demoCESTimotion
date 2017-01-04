aeaa1ab026e5fb3116afed73849e58fc7e25a741
quangthanh010290 <thanh@autonomous.nyc>
2017-01-04 07:27:21 -0800

    rm

--- hostCP.sh
+++
@@ -1,3 +0,0 @@
-rm -rf deskrep libiComm.so
-sudo cp /usr/lib/libiComm.so ~/ws/demoCESTimotion
-cp /home/thanh/aos/system/os_manager/smartdesk/deskApp/TimotionV1/user/RPI/deskrep  ~/ws/demoCESTimotion
--- setup.sh
+++
@@ -1,3 +0,0 @@
-sudo cp libiComm.so /usr/lib
-cp deskrep ~/aos/system/SMART_DESK/deskApp/deskRep
-cp data.dat  ~/aos/system/SMART_DESK/deskApp/deskRep
